7f3fe7a2f464d9e8e6f8711c652e663233e0f883
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-09-24 00:11:41 +0700

    "update convert"

--- app/Console/Commands/ConvertCommand.php
+++ app/Console/Commands/ConvertCommand.php
@@ -2,8 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\WPPost;
-use App\Repositories\WPPostRepository;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
@@ -14,7 +12,7 @@ class ConvertCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'convert:post';
+    protected $signature = 'convert:data';
 
     /**
      * The console command description.
@@ -40,6 +38,16 @@ class ConvertCommand extends Command
      */
     public function handle()
     {
+        $table = 'wp_yoast_indexable';
+
+        DB::table($table)->orderBy('id')->chunk(100, function ($data) use ($table) {
+            foreach ($data as $item) {
+                $new_url = str_replace("https://kungfucongnghe.com", "http://localhost/develop/kungfucongnghe", $item->permalink);
+                DB::table($table)->where('id', $item->id)->update(['permalink' => $new_url]);
+                dump("DONE: " . $new_url);
+            }
+        });
+
         return "SUCCESS";
     }
 }
