dc244a45d6f9090c187afd1515a0c963357d811a
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-01 00:42:17 +0700

    "update crawl"

--- app/Console/Commands/ConvertSeoCommand.php
+++ app/Console/Commands/ConvertSeoCommand.php
@@ -40,34 +40,37 @@ class ConvertSeoCommand extends Command
     public function handle()
     {
         $table = 'wp_posts';
-        DB::table($table)->orderBy('ID')->chunk(100, function ($data) use ($table) {
-            foreach ($data as $post) {
+        DB::table($table)
+            ->whereNotNull('url_crawl')
+            ->where('post_status', 'private')
+            ->orderBy('ID')
+            ->chunk(100, function ($data) use ($table) {
+                foreach ($data as $post) {
+                    $data_yoast_indexable = [
+                        'title' => Helper::handleKeySeo($post->post_title),
+                        'primary_focus_keyword' => Helper::handleKeySeo($post->post_title),
+                    ];
+                    DB::table('wp_yoast_indexable')->where(
+                        [
+                            ['object_id', $post->ID],
+                            ['object_type', 'post'],
+                            ['object_sub_type', 'post'],
+                        ]
+                    )->update($data_yoast_indexable);
 
-                $data_yoast_indexable = [
-                    'title' => Helper::handleKeySeo($post->post_title),
-                    'primary_focus_keyword' => Helper::handleKeySeo($post->post_title),
-                ];
-                DB::table('wp_yoast_indexable')->where(
-                    [
-                        ['object_id', $post->ID],
-                        ['object_type', 'post'],
-                        ['object_sub_type', 'post'],
-                    ]
-                )->update($data_yoast_indexable);
+                    $data_postmeta = [
+                        'meta_value' => Helper::handleKeySeo($post->post_title),
+                    ];
+                    DB::table('wp_postmeta')->where(
+                        [
+                            ['post_id', $post->ID],
+                            ['meta_key', '_yoast_wpseo_focuskw']
+                        ]
+                    )->update($data_postmeta);
 
-                $data_postmeta = [
-                    'meta_value' => Helper::handleKeySeo($post->post_title),
-                ];
-                DB::table('wp_postmeta')->where(
-                    [
-                        ['post_id', $post->ID],
-                        ['meta_key', '_yoast_wpseo_focuskw']
-                    ]
-                )->update($data_postmeta);
-
-                dump(Helper::handleKeySeo($post->post_title) . "\n");
-            }
-        });
+                    dump(Helper::handleKeySeo($post->post_title) . "\n");
+                }
+            });
         return "SUCCESS";
     }
 }
